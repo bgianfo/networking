a205be47ca20c1212c3aa0037a2975755ffdeed3
Brian Gianforcaro <b.gianfo@gmail.com>
2010-09-20 11:04:41 -0400

    Additional documentation, code formatting, and splitting constants into a new header.

--- client/client.cpp
+++ client/client.cpp
@@ -1,63 +1,33 @@
 /**
  * Author: Brian Gianforcaro ( bjg1955@cs.rit.edu )
  *
- * Description: 
+ * Description: A client appiication that can add, retrive
+ * records from a remote database server. 
  *
  * Usage: tcp-project1 hostname port
  * 				
- * 				Where 'hostname' is the name of the remote host on which
- * 				the server is running and 'port' is the port number it is using.
+ * Where 'hostname' is the name of the remote host on which
+ * the server is running and 'port' is the port number it is using.
  */
 
+// Stream stdout/stderr IO
 #include <iostream>
   using std::cout;
   using std::cerr;
   using std::endl;
 
-#include <string>
-	using std::string;
-
+// Utilities, IO and Error checking
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <strings.h> // for bzero(..)
 #include <string.h>
 
-
-#include <errno.h>
+// Networking and sockets
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
-#define	PORT_MIN	1024
-#define	PORT_MAX	65535
-
-#define HOSTNAME 1
-#define PORTNUM 2
-
-#define ADD_SUCCESS 0
-#define ADD_FAILURE 1
-
-#define RET_SUCCESS 0
-#define RET_FAILURE 1
-
-
-#define MAX_LEN 32
-
-typedef struct { 
-  int command; 
-  int id; 
-	char name[MAX_LEN]; 
-  int   age;
-} record_t;
-
-
-enum {
-	add_t,
-	retrive_t,
-	quit_t
-} actions_t;
-
 
 /**
  * Setup the connection to the server and return the sockets file descriptor.
@@ -69,38 +39,37 @@ enum {
  */
 int setupSocket( char* hostname, int port )
 {
+  // Initialize a socket to work with
+  int sock = socket( AF_INET, SOCK_STREAM, 0 );
+  if ( sock < 0 )
+  {
+    cerr << "socket: " << strerror( errno ) << endl;
+    exit( EXIT_FAILURE );
+  }
 
-	// Initialize a socket to work with
-	int sock = socket( AF_INET, SOCK_STREAM, 0 );
-	if ( sock < 0 )
-	{
-		cerr << "socket: " << strerror( errno ) << endl;
-		exit( EXIT_FAILURE );
-	}
+  // Resolve the host-name to an address
+  struct hostent *hostent = gethostbyname( hostname );
+  if ( NULL == hostent )
+  {
+    cerr << "gethostbyname: error code " << h_errno << endl;
+    exit( EXIT_FAILURE );
+  }
 
-	// Resolve the host-name to an address
-	struct hostent *hostent = gethostbyname( hostname );
-	if ( hostent == NULL )
-	{
-		cerr << "gethostbyname: error code " << h_errno << endl;
-		exit( EXIT_FAILURE );
-	}
+  // Setup socket address struct
+  struct sockaddr_in address;
+  bzero( &address, sizeof( address ) );
+  address.sin_family = AF_INET;
+  address.sin_port = htons( port );
+  address.sin_addr.s_addr = *(u_long *)hostent->h_addr;
 
-	struct sockaddr_in address;
-	bzero( &address, sizeof( address ) );
+  // Connect the socket to the previously setup address
+  if ( connect( sock, (struct sockaddr*)&address, sizeof( address ) ) < 0 )
+  {
+    cerr << "connect: " << strerror( errno ) << endl;
+    exit( EXIT_FAILURE );
+  }
 
-	address.sin_family = AF_INET;
-	address.sin_port = htons( port );
-	address.sin_addr.s_addr = *(u_long *)hostent->h_addr;
-
-	// Connect the socket to the previously setup address
-	if ( connect( sock, (struct sockaddr *)&address, sizeof( address ) ) < 0 )
-	{
-		cerr << "connect: " << strerror( errno ) << endl;
-		exit( EXIT_FAILURE );
-	}
-
-	return sock;
+  return sock;
 }
 
 /**
@@ -110,171 +79,187 @@ int setupSocket( char* hostname, int port )
  */
 void usage( char* binary )
 {
-	cerr << "Usage: " << binary << "hostname port " << endl;
+  cerr << "Usage: " << binary << "hostname port " << endl;
 }
 
+/**
+ * Attempt to add a new record to the remote database.
+ *
+ * @param[in] sock - The socket's file descriptor
+ */
 void addRecord( int sock )
 {
-	record_t newRecord;
-	cout << "Enter id (interger):";
 
-	newRecord.command = add_t;
+  record_t newRecord;
+  cout << "Enter id (interger):";
 
-	bool valid = false;
-	while( not valid )
-	{
-		if ( scanf( "%d", &newRecord.id ) == EOF )
-		{
-			cout << "ID should be a non-zero integer):";
-		} 
-		else if ( newRecord.id == 0 ) 
-		{
-			cout << "ID should be a non-zero integer):";
-		}
-		else
-		{
-			valid = true;
-		}
-	}
+  newRecord.command = add_t;
 
-	cout << "Enter name (up to 32 char):";
-	scanf( "%32s", newRecord.name );
+  bool valid = false;
+  while ( not valid )
+  {
+    if ( scanf( "%d", &newRecord.id ) == EOF )
+    {
+      cerr << "ID should be a non-zero integer):";
+    } 
+    else if ( newRecord.id == 0 ) 
+    {
+      cerr << "ID should be a non-zero integer):";
+    }
+    else
+    {
+      valid = true;
+    }
+  }
 
-	cout << "Enter age (integer):";
-	valid = false;
-	while( not valid )
-	{
-		if ( scanf( "%d", &newRecord.age ) == EOF )
-		{
-			cout << "Age should be a non-zero integer):";
-		} 
-		else if ( newRecord.age == 0 ) 
-		{
-			cout << "Age should be a non-zero integer):";
-		}
-		else
-		{
-			valid = true;
-		}
-	}
+  cout << "Enter name (up to 32 char):";
+  scanf( "%32s", newRecord.name );
 
-	// Now do the actual writing of the data out to the socket.
-	write( sock, (char*) &newRecord, sizeof(newRecord) );
+  cout << "Enter age (integer):";
+  valid = false;
+  while ( not valid )
+  {
+    if ( scanf( "%d", &newRecord.age ) == EOF )
+    {
+      cerr << "Age should be a non-zero integer):";
+    } 
+    else if ( newRecord.age == 0 ) 
+    {
+      cerr << "Age should be a non-zero integer):";
+    }
+    else
+    {
+      valid = true;
+    }
+  }
 
-	record_t resultRec;
-	bzero( &resultRec, sizeof( resultRec ) );
+  // Now do the actual writing of the data out to the socket.
+  write( sock, (char*) &newRecord, sizeof(newRecord) );
 
-	read( sock, (char*) &resultRec, sizeof(resultRec) );
+  record_t resultRec;
+  bzero( &resultRec, sizeof( resultRec ) );
 
-	if ( resultRec.command == ADD_SUCCESS )
-	{
-		cout << "ID " << newRecord.id << " added successfully" << endl;
-	}
-	else
-	{
-		cout << "ID " << newRecord.id << " already exists" << endl;
-	}
+  read( sock, (char*) &resultRec, sizeof(resultRec) );
 
-
+  if ( resultRec.command == ADD_SUCCESS )
+  {
+    cout << "ID " << newRecord.id << " added successfully" << endl;
+  }
+  else
+  {
+    cout << "ID " << newRecord.id << " already exists" << endl;
+  }
 }
 
+
+/**
+ * Attempt to retrive a record from the remote database.
+ *
+ * @param[in] sock - The socket's file descriptor
+ */
 void retriveRecord( int sock )
 {
-	record_t findRecord;
-	findRecord.command = retrive_t;
+  record_t findRecord;
+  findRecord.command = retrive_t;
 
-	cout << "Enter id (interger):";
+  cout << "Enter id (interger):";
 
-	bool valid = false;
-	while( not valid )
-	{
-		if ( scanf( "%d", &findRecord.id ) == EOF )
-		{
-			cout << "ID should be a non-zero integer):";
-		} 
-		else if ( findRecord.id == 0 ) 
-		{
-			cout << "ID should be a non-zero integer):";
-		}
-		else
-		{
-			valid = true;
-		}
-	}
+  bool valid = false;
+  while ( not valid )
+  {
+    if ( scanf( "%d", &findRecord.id ) == EOF )
+    {
+      cout << "ID should be a non-zero integer):";
+    } 
+    else if ( findRecord.id == 0 ) 
+    {
+      cout << "ID should be a non-zero integer):";
+    }
+    else
+    {
+      valid = true;
+    }
+  }
 
-	// Now do the actual writing of the data out to the socket.
-	write( sock, (char*) &findRecord, sizeof(findRecord) );
+  // Now do the actual writing of the data out to the socket.
+  write( sock, (char*) &findRecord, sizeof(findRecord) );
 
-	record_t resultRec;
-	bzero( &resultRec, sizeof( resultRec ) );
 
-	read( sock, (char*) &resultRec, sizeof(resultRec) );
+  record_t resultRec;
+  bzero( &resultRec, sizeof( resultRec ) );
 
-	if ( resultRec.command == RET_SUCCESS )
-	{
-		cout << "ID: " << resultRec.id << endl;
-		cout << "Name: " << resultRec.name << endl;
-		cout << "Age: " << resultRec.age << endl;
-	}
-	else
-	{
-		cout << "ID " << findRecord.id << " does not exist" << endl;
-	}
+  // Get the result back from the server
+  read( sock, (char*) &resultRec, sizeof(resultRec) );
 
+  // Display our results
+  if ( resultRec.command == RET_SUCCESS )
+  {
+    cout << "ID: " << resultRec.id << endl;
+    cout << "Name: " << resultRec.name << endl;
+    cout << "Age: " << resultRec.age << endl;
+  }
+  else
+  {
+    cout << "ID " << findRecord.id << " does not exist" << endl;
+  }
 }
 
 
 
+/**
+ * Client main function
+ *
+ * @param[in] argc - The number of arguments passed to the program.
+ * @param[in] argv - The arguments passed to the program.
+ */
 int main( int argc, char** argv )
 {
 
-	if ( argc != 3 )
-	{	
-		usage( argv[0] );
-		return EXIT_FAILURE;
-	}
-	else
-	{
+  if ( argc != 3 )
+  {	
+    usage( argv[0] );
+    return EXIT_FAILURE;
+  }
+  else
+  {
+    char* hostname = argv[HOSTNAME];
+    int port = atoi( argv[PORTNUM] );
 
-		char* hostname = argv[HOSTNAME];
-		int port = atoi( argv[PORTNUM] );
+    // Validate the given port number
+    if ( port < PORT_MIN || port > PORT_MAX )
+    {
+      cerr << port << ": invalid port number" << endl;
+      exit( EXIT_FAILURE );
+    }
+    
+    int sock = setupSocket( hostname, port );
 
-		// Validate the port number chosen
-		if ( port < PORT_MIN || port > PORT_MAX )
-		{
-			cerr << port << ": invalid port number" << endl;
-			exit( EXIT_FAILURE );
-		}
-		
-		int sock = setupSocket( hostname, port );
+    bool shouldQuit = false;
+    while ( not shouldQuit )	
+    {
 
-		bool shouldQuit = false;
-	  while ( not shouldQuit )	
-		{
+      cout << "Enter command (" << add_t << " for Add, " << retrive_t 
+           << " for Retrive, " << quit_t << " to quit):"; 
 
-			cout << "Enter command (" << add_t << " for Add, " << retrive_t 
-					 << " for Retrive, " << quit_t << " to quit):"; 
-
-			int cmd; 
-			scanf( "%d", &cmd );
-			
-			switch ( cmd )
-			{
-				case add_t:
-					addRecord( sock );
-					break;
-				case retrive_t:
-					retriveRecord( sock );
-					break;
-				case quit_t:
-					close( sock );
-					shouldQuit = true;
-					break;
-				default:
-					break;
-			}
-		}
-
-		return EXIT_SUCCESS;
-	}
+      int cmd; 
+      scanf( "%d", &cmd );
+      
+      switch ( cmd )
+      {
+        case add_t:
+          addRecord( sock );
+          break;
+        case retrive_t:
+          retriveRecord( sock );
+          break;
+        case quit_t:
+          close( sock );
+          shouldQuit = true;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return EXIT_SUCCESS;
 }
